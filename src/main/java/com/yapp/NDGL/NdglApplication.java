bc7138addf7673031c7600baa3197feccb0dbe34
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-12-30 01:29:54 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/yapp/NDGL/NdglApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.NDGL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yapp/NDGL/NdglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.NDGL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
