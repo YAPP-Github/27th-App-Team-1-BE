af5947516934b61045c93d2cc84c5e84a9953d9f
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-01-06 20:01:43 +0900

    fix: COMMON 에러 코드 수정

--- common/src/main/java/com/yapp/ndgl/common/exception/CommonErrorCode.java
+++ common/src/main/java/com/yapp/ndgl/common/exception/CommonErrorCode.java
@@ -13,8 +13,8 @@ public enum CommonErrorCode implements BaseErrorCode {
 
 	MISSING_REQUEST_HEADER(StatusCode.BAD_REQUEST, DomainCode.COMM,
 		CategoryCode.INPUT_VALIDATION, "001", "필수 헤더가 존재하지 않습니다"),
-	TYPE_MISS_MATCH_ERRORS_IN_REQUEST_BODY(StatusCode.BAD_REQUEST, DomainCode.COMM,
-		CategoryCode.INPUT_VALIDATION, "002", "허용되지 않는 필드입니다."),
+	INVALID_REQUEST_BODY(StatusCode.BAD_REQUEST, DomainCode.COMM,
+		CategoryCode.INPUT_VALIDATION, "002", "요청 본문을 읽을 수 없거나 형식이 올바르지 않습니다."),
 	TYPE_MISS_MATCH_ERRORS_IN_REQUEST_PARAM_DATA(StatusCode.BAD_REQUEST, DomainCode.COMM,
 		CategoryCode.INPUT_VALIDATION, "003", "허용되지 않는 쿼리입니다."),
 	METHOD_NOT_ALLOWED(StatusCode.METHOD_NOT_ALLOWED, DomainCode.COMM,
@@ -22,11 +22,15 @@ public enum CommonErrorCode implements BaseErrorCode {
 
 	VALIDATION_ERRORS_IN_REQUEST_DATA(StatusCode.UNPROCESSABLE_ENTITY, DomainCode.COMM,
 		CategoryCode.INPUT_VALIDATION, "005", "유효성 검증에 실패하였습니다"),
+	MISSING_REQUEST_PARAMETER(StatusCode.BAD_REQUEST, DomainCode.COMM,
+		CategoryCode.INPUT_VALIDATION, "006", "필수 요청 파라미터가 존재하지 않습니다"),
 
 	/**
 	 * COMM-02-xxx
 	 * RESOURCE_NOT_FOUND
 	 */
+	NOT_FOUND_URI(StatusCode.NOT_FOUND, DomainCode.COMM,
+		CategoryCode.RESOURCE_NOT_FOUND, "001", "요청하신 URI를 찾을 수 없습니다"),
 
 	/**
 	 * COMM-03-xxx
@@ -59,6 +63,8 @@ public enum CommonErrorCode implements BaseErrorCode {
 	 * COMM-08-xxx
 	 * INTERNAL_SYSTEM
 	 */
+	INTERNAL_SERVER_ERROR(StatusCode.INTERNAL_SERVER_ERROR, DomainCode.COMM,
+		CategoryCode.INTERNAL_SYSTEM, "001", "서버 내부 오류가 발생했습니다")
 
 	;
 
