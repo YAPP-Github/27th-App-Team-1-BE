f282ebd85e6765c9318d34179eb8076a5d096b67
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-01-12 20:21:37 +0900

    chore: API 문서화 도구 추가 (Swagger) (#6)
    * chore: CLAUDE context 정보 ignore 추가
    * chore: swagger 설정
    * refactor: application 확장자 변경 (yaml -> yml)
    * chore: coderabbit 비활성화
    * chore: coderabbit 활성화
    
    closed #6

---
+++ application/src/main/java/com/yapp/ndgl/application/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.yapp.ndgl.application.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NDGL API")
+                        .description("NDGL API 문서")
+                        .version("v1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi
+            .builder()
+            .group("api")
+            .packagesToScan("com.yapp.ndgl.application.*")
+            .build();
+    }
+}
