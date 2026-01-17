62dda8a50d0944d6d02b305c3f3c7e426074f9d8
Huey-J <wkdgus7113@gmail.com>
2026-01-17 10:39:03 +0900

    feat: path 에 API Version 추가

--- application/src/main/java/com/yapp/ndgl/application/temp/controller/TempController.java
+++ application/src/main/java/com/yapp/ndgl/application/temp/controller/TempController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Tag(name = "Test", description = "테스트 API")
 @RestController
-@RequestMapping("/api/temp")
+@RequestMapping("/api/v1/temp")
 public class TempController {
 
   @Operation(
