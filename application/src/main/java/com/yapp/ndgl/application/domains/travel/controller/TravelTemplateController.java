af5d7eca3a1e8fadc43e7cb98a4919b46d61f373
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-01-22 02:02:59 +0900

    feat: 여행 템플릿 상단 정보 조회 api 분리

--- application/src/main/java/com/yapp/ndgl/application/domains/travel/controller/TravelTemplateController.java
+++ application/src/main/java/com/yapp/ndgl/application/domains/travel/controller/TravelTemplateController.java
@@ -1,24 +1,39 @@
 package com.yapp.ndgl.application.domains.travel.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yapp.ndgl.application.domains.travel.controller.dto.TravelTemplateHighlightsResponse;
 import com.yapp.ndgl.application.domains.travel.controller.dto.TravelTemplateResponse;
 import com.yapp.ndgl.application.domains.travel.facade.TravelTemplateFacade;
 import com.yapp.ndgl.common.response.SuccessResponse;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/travel-templates")
 @RestController
 public class TravelTemplateController implements TravelTemplateApi {
 
     private final TravelTemplateFacade travelTemplateFacade;
 
     @Override
+    @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse<TravelTemplateResponse>> getTravelTemplate(
         @PathVariable Long id
     ) {
         TravelTemplateResponse response = travelTemplateFacade.getTravelTemplate(id);
+        return ResponseEntity.ok(SuccessResponse.success(response));
+    }
+
+    @GetMapping("/{id}/content-card")
+    public ResponseEntity<SuccessResponse<TravelTemplateHighlightsResponse>> readTravelTemplateHighlights(
+        @PathVariable("id") final Long id
+    ) {
+        TravelTemplateHighlightsResponse response = travelTemplateFacade.readTravelTemplateHighlights(id);
         return ResponseEntity.ok(SuccessResponse.success(response));
     }
 }
--- application/src/main/java/com/yapp/ndgl/application/domains/travel/facade/TravelTemplateFacade.java
+++ application/src/main/java/com/yapp/ndgl/application/domains/travel/facade/TravelTemplateFacade.java
@@ -1,10 +1,14 @@
 package com.yapp.ndgl.application.domains.travel.facade;
 
 import com.yapp.ndgl.application.common.annotation.Facade;
+import com.yapp.ndgl.application.domains.travel.controller.dto.TravelTemplateHighlightsResponse;
 import com.yapp.ndgl.application.domains.travel.controller.dto.TravelTemplateResponse;
 import com.yapp.ndgl.application.domains.travel.service.TravelTemplateService;
-import lombok.RequiredArgsConstructor;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Facade
 @RequiredArgsConstructor
 public class TravelTemplateFacade {
@@ -13,5 +17,10 @@ public class TravelTemplateFacade {
 
     public TravelTemplateResponse getTravelTemplate(Long id) {
         return travelTemplateService.getTravelTemplate(id);
+    }
+
+    public TravelTemplateHighlightsResponse readTravelTemplateHighlights(final Long id) {
+        log.info("여행 템플릿의 상단 내역을 조회합니다 template id = {}", id);
+        return travelTemplateService.readTravelTemplateHighlights(id);
     }
 }
