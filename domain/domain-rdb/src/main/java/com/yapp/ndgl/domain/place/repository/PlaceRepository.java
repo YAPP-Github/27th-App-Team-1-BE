07055ace97c7dd8a6ccda19f37c53d13f67d8513
Huey-J <wkdgus7113@gmail.com>
2026-02-24 00:20:04 +0900

    NDGL-117 내 여행 일정 일괄 수정 API Request ID 변경

--- domain/domain-rdb/src/main/java/com/yapp/ndgl/domain/place/repository/PlaceRepository.java
+++ domain/domain-rdb/src/main/java/com/yapp/ndgl/domain/place/repository/PlaceRepository.java
@@ -1,5 +1,6 @@
 package com.yapp.ndgl.domain.place.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,8 @@ import com.yapp.ndgl.domain.place.entity.PlaceEntity;
 public interface PlaceRepository extends JpaRepository<PlaceEntity, Long> {
 
 	Optional<PlaceEntity> findByGooglePlaceId(final String googlePlaceId);
+
+	List<PlaceEntity> findByGooglePlaceIdIn(final List<String> googlePlaceIds);
 
 	boolean existsByGooglePlaceId(final String googlePlaceId);
 
