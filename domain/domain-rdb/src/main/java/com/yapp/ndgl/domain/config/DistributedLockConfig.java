c109fd88ce608cd705e62b380f55abb60c065864
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-05-09 00:50:29 +0900

    feat: [NDGL-139] DistributedLockConfig Bean 등록 구현

--- domain/domain-rdb/src/main/java/com/yapp/ndgl/domain/config/DistributedLockConfig.java
+++ domain/domain-rdb/src/main/java/com/yapp/ndgl/domain/config/DistributedLockConfig.java
@@ -1,4 +1,18 @@
 package com.yapp.ndgl.domain.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.yapp.ndgl.domain.common.lock.NamedLockRepository;
+import com.yapp.ndgl.lock.DistributedLockRepository;
+
+@Configuration
 public class DistributedLockConfig {
+
+	@Bean
+	public DistributedLockRepository distributedLockRepository(final DataSource dataSource) {
+		return new NamedLockRepository(dataSource);
+	}
 }
