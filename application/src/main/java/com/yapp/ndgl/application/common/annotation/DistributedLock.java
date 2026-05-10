e1e1ed52bcf332f407d35a46d1081e0c791401ad
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-05-10 18:09:05 +0900

    chore: 락 획득 대기 시간 javadoc 추가

--- application/src/main/java/com/yapp/ndgl/application/common/annotation/DistributedLock.java
+++ application/src/main/java/com/yapp/ndgl/application/common/annotation/DistributedLock.java
@@ -9,7 +9,13 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
 
+    /**
+     * 락 키를 생성하는 SpEL 표현식.
+     */
     String key();
 
+    /**
+     * 락 획득 대기 시간 (단위: 초).
+     */
     int timeout() default 3;
 }
