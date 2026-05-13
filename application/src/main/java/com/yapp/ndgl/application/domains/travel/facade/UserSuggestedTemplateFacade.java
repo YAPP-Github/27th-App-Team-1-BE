6020b1ac5865de08ed92b544e1969e544987f032
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-05-13 16:37:59 +0900

    feat: [NDGL-133] 어드민 사용자 제안 템플릿 목록 조회 페이지 추가

--- application/src/main/java/com/yapp/ndgl/application/domains/travel/facade/UserSuggestedTemplateFacade.java
+++ application/src/main/java/com/yapp/ndgl/application/domains/travel/facade/UserSuggestedTemplateFacade.java
@@ -1,13 +1,18 @@
 package com.yapp.ndgl.application.domains.travel.facade;
 
 import com.yapp.ndgl.application.common.annotation.Facade;
+import com.yapp.ndgl.application.domains.travel.controller.dto.AdminUserSuggestedTemplateResponse;
 import com.yapp.ndgl.application.domains.travel.controller.dto.CreateUserSuggestedTemplateRequest;
 import com.yapp.ndgl.application.domains.travel.event.publisher.UserSuggestedTemplateEventPublisher;
 import com.yapp.ndgl.application.domains.travel.service.UserSuggestedTemplateService;
 import com.yapp.ndgl.application.utils.YoutubeUrlParser;
+import com.yapp.ndgl.common.response.PageResponse;
+import com.yapp.ndgl.common.type.SuggestionStatus;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Facade
 @RequiredArgsConstructor
 public class UserSuggestedTemplateFacade {
@@ -27,5 +32,12 @@ public class UserSuggestedTemplateFacade {
 
     public void subscribe(final Long templateId, final String uuid) {
         userSuggestedTemplateService.subscribe(templateId, uuid);
+    }
+
+    public PageResponse<AdminUserSuggestedTemplateResponse> readUserSuggestedTemplatesForAdmin(
+        final SuggestionStatus status, final int page, final int size
+    ) {
+        log.info("어드민 사용자 제안 템플릿 목록을 조회합니다. status = {}, page = {}, size = {}", status, page, size);
+        return userSuggestedTemplateService.readUserSuggestedTemplatesForAdmin(status, page, size);
     }
 }
